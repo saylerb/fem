b2da4db4477e6497605c6d9acde4d984e0d0d4e9
Brian Sayler <sayler.b@gmail.com>
2019-07-04 11:13:07 -0500

    render the current number in the display

--- exercise-two/calculator.js
+++ exercise-two/calculator.js
@@ -1,31 +1,52 @@
-// Event delegation
-
 const calculator = document.querySelector(".calculator");
 const debugText = document.getElementById("debug");
 
 let commands = [];
+let displayChars = [];
 
 calculator.addEventListener("click", event => {
   const buttonText = event.target.innerHTML;
 
   if (event.target.tagName === "BUTTON") {
-    commands = updateCommands(buttonText, commands);
+    [commands, displayChars] = update(buttonText, commands, displayChars);
   }
 
+  renderDisplay(displayChars);
+
   debugText.innerHTML =
-    event.target.tagName + "\nCommands:" + JSON.stringify(commands, null, 2);
+    event.target.tagName +
+    "\nCommands:" +
+    JSON.stringify(commands, null, 2) +
+    "\nDisplay:" +
+    JSON.stringify(displayChars, null, 2);
 });
 
-function updateCommands(buttonText, commands) {
-  const updated = commands.concat(buttonText);
+function update(buttonText, commands, displayChars) {
+  const updatedCommands = commands.concat(buttonText);
+  const updatedDisplay = [...displayChars];
 
   const isNumber = !Number.isNaN(parseInt(buttonText));
 
   if (!isNumber) {
     if (buttonText === "C") {
-      return [];
+      return [[], []];
+    } else if (buttonText === "←") {
+      return [
+        updatedCommands,
+        updatedDisplay.splice(0, updatedDisplay.length - 1)
+      ];
+    } else {
+      return [updatedCommands, []]; // An operator was pressed
     }
+  } else {
+    updatedDisplay.push(buttonText);
   }
 
-  return updated;
+  return [updatedCommands, updatedDisplay];
+}
+
+function renderDisplay(displayChars) {
+  const display = document.querySelector(".display");
+
+  display.innerHTML = displayChars.join("");
 }
