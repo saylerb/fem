4696134ab6d93b49be97179c99352e105b4f8c06
Brian Sayler <sayler.b@gmail.com>
2019-07-05 09:02:23 -0500

    create express server

---
+++ node/data.js
@@ -0,0 +1,13 @@
+const complements = [
+  "You like nice today",
+  "That dress looks nice on you",
+  "Have you been working out?",
+  "You can do hard things",
+  "You've gotten far in this course. You're really smart",
+  "You're programming! How cool is that?",
+  "I'm really proud of you",
+  "You made this",
+  "You've learned a lot of things, and that's pretty hard to do"
+];
+
+module.exports = complements;
--- node/server.js
+++ node/server.js
@@ -1,9 +1,27 @@
-const http = require("http");
+const express = require("express");
+const path = require("path");
+const complements = require("./data");
 
-const server = http.createServer(function(req, res) {
-  console.log(`user visited ${req.url}`);
-  res.end("hello!");
+const app = express();
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/complement", function(req, res) {
+  res
+    .json({
+      complement: getRandomComplement()
+    })
+    .end();
+});
+
+app.use("/public", express.static("./public"));
+
+app.listen(3000);
 console.log("listening on http://localhost:3000");
-server.listen(3000);
+
+function getRandomComplement() {
+  const randomIndex = Math.floor(Math.random() * complements.length);
+  return complements[randomIndex];
+}
